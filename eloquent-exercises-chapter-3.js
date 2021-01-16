245931eb378851efaa5f35fbfbcb2903abff81df
Nathan Cho <nat11374@esri.com>
2021-01-15 17:51:34 -0800

    Update eloquent-exercises-chapter-3.js

---
+++ eloquent-exercises-chapter-3.js
@@ -0,0 +1,43 @@
+// Eloquent JavaScript Exercises
+
+// CHAPTER 2: FUNCTIONS
+
+// ***
+
+// MINIMUM
+
+// Build a function that returns its smallest argument
+
+// Define min to hold a function value
+const min = function(a, b) {
+	if (a < b) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+// Declare g to be a function
+function min(c, d) {
+	if (c < d) {
+		return c;
+	} else {
+		return d;
+	}
+}
+
+// A less verbose function value
+min = (e, f) => {
+	if (e < f) {
+		return e;
+	} else {
+		return f;
+	}
+}
+
+// ***
+
+// RECURSION
+
+
+
