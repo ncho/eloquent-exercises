b6e330a33303baa814d268c5e0640cf69448b322
Nathan Cho <ncho@esri.com>
2022-12-09 12:09:11 -0800

    Update eloquent-exercises-chapter-4.js

---
+++ eloquent-exercises-chapter-4.js
@@ -0,0 +1,30 @@
+// Eloquent JavaScript Exercises
+
+// CHAPTER 2: FUNCTIONS
+
+// ---
+
+// THE SUM OF A RANGE
+
+console.log(sum(range(1, 10)));
+
+function range(start, end) {
+
+// Need to count up from start to end
+// Add every number counted to an array
+// Stop counting when reaching end
+// Return array countaining all numbers from start up to (and inlcuding) end
+
+}
+
+// Use a for loop for `sum`? 
+
+function sum(/* array */) {
+
+// Take value at index position 0, add to a count
+// Add to that, index++
+// Stop adding when reaching end of array
+// Return sum 
+
+}
+
